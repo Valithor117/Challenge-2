9bc68526bc84dfe61600b714a23f123f0b1cad52
Valithor117 <46632658+Valithor117@users.noreply.github.com>
2019-03-10 15:50:52 -0400

    Added Files
    yay

---
+++ Assets/Scripts/Lerp1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerp1 : MonoBehaviour
+{
+    public Transform Start;
+
+    public Transform End;
+     
+
+    private void Update()
+    {
+         transform.position = Vector2.Lerp(Start.position, End.position, Mathf.PingPong(Time.time, 1.0f));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
+    public float speed;
+    public float jumpForce;
+    public Text countText;
+    public Text livesText;
+    public Text winText;
+    public Text loseText;
+    public AudioClip BackMusic;
+    public AudioClip WinMusic;
+    public AudioSource musicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+        count = 0;
+
+        SetCountText();
+
+        countText.text = "";
+
+        lives = 4;
+
+        SetLivesText();
+
+        winText.text = "";
+
+        loseText.text = "";
+
+        
+               
+    }
+
+    private void Awake()
+    {
+        musicSource.clip = BackMusic;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (lives == 0)
+        { Destroy(gameObject); }
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+
+        if(Input.GetKey(KeyCode.Escape))
+        { Application.Quit(); }
+
+        
+
+        if (count == 8)
+        {
+            musicSource.clip = WinMusic;
+            musicSource.Play();
+        }
+        if (count == 8)
+        {
+            count++;
+        }
+
+        
+       
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+
+            count = count + 1;
+
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+
+            lives = lives - 1;
+
+            SetLivesText();
+
+         }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 8)
+            winText.text = "You Win!";
+
+        if (count == 4)
+        { transform.position = new Vector3(20, 0, 0.0f); }
+
+    }
+    
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+
+        if (lives == 0)
+            loseText.text = "You Lose.";
+
+    }
+ }
